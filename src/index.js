0c0cfa5a8cfaff44398241a5611769b59a313405
nguyenphuthinh1025 <nguyenphuthinh1025@gmail.com>
2022-09-05 14:53:17 +0700

    upload-code

--- src/index.js
+++ src/index.js
@@ -3,13 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css';
+import { Provider } from 'react-redux'
+import { store } from './redux/configStore';
+
+import { DOMAIN } from './util/setting';
+import * as signalR from '@aspnet/signalr';
+
+import './i18n';
+
+export const connection = new signalR.HubConnectionBuilder().withUrl(`${DOMAIN}/DatVeHub`).configureLogging(signalR.LogLevel.Information).build();
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+connection.start().then(() => {
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}).catch((error) => {
+  console.log(error);
+})
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
